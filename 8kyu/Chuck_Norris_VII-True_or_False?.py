05d0ff0760b16dd09983fa1ab4ff9893557dbc29
roman.tsakunov <roman.tsakunov@corp.mail.ru>
2023-05-25 13:03:53 +0300

    add new kata

---
+++ 8kyu/Chuck_Norris_VII-True_or_False?.py
@@ -0,0 +1,9 @@
+"""
+Chuck Norris VII - True or False?(https://www.codewars.com/kata/570669d8cb7293a2d1001473/train/python)
+
+Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
+"""
+
+
+def if_chuck_says_so():
+    return not (True)
